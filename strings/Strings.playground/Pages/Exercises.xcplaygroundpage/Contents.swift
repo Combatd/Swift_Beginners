86d03372829b2178a50578d12459afc77bf3d9df
Combatd <combatdstar@gmail.com>
2020-09-21 09:02:19 -0700

    Solve Strings exercises

--- strings/Strings.playground/Pages/Exercises.xcplaygroundpage/Contents.swift
+++ strings/Strings.playground/Pages/Exercises.xcplaygroundpage/Contents.swift
@@ -6,7 +6,7 @@ import Foundation
 //: Recreate the shoutString by using the didYouKnowString as a stem.
 let didYouKnowString = "Did you know that Swift strings come with lots of useful functions?"
 let whisperString = "psst" + ", " + didYouKnowString.lowercased()
-let shoutString =  "HEY! DID YOU KNOW THAT SWIFT STRINGS COME WITH LOTS OF USEFUL FUNCTIONS?"
+let shoutString =  "HEY! \(didYouKnowString.uppercased())"
 
 //: ### Exercise 2
 //: How many characters are in this string?
@@ -15,9 +15,12 @@ let howManyCharacters = "How much wood could a woodchuck chuck if a woodchuck co
 //: ### Exercise 3
 //: Reverse the characters in <word> and print out the result.
 let word = "deliver"
-
+// Convert the array to a String
+let reversedWord = String(word.reversed())
+print(reversedWord)
 //: Exercise 4
 //: Replace the o's in octopus with the octopus emoji.
 let animal = "octopus"
-
+let octopusAnimal = animal.replacingOccurrences(of: "o", with: "🐙")
+print(octopusAnimal)
 //: [Next](@next)
